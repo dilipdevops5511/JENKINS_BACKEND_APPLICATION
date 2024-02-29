382088a88310af0a7513b7093d0610195ecd05bc
dilipdevops5511 <dilipdevopscourses@gmail.com>
2024-02-29 08:43:34 +0530

    first commit

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 import dotenv from 'dotenv';
 
-dotenv.config({path: './config.env'});
-
-console.log(process.env.DB_USERNAME);
-console.log(process.env.DB_PASS);
 
 
-export const PORT = 5000;
-export const mongoDBURL =`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.n7msjwt.mongodb.net/?retryWrites=true&w=majority`;
+
+export const PORT = 5555;
+export const mongoDBURL =`mongodb+srv://app:admin@cluster0.n7msjwt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
